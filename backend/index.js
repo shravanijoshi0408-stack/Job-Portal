792680a850bf903661cd64b5eccfddd3f3394737
shravanijoshi0408-stack <shravanijoshi0408@gmail.com>
2026-06-19 22:02:47 +0530

    Add Vercel frontend to CORS

--- backend/index.js
+++ backend/index.js
@@ -21,9 +21,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
+    origin: [
+        "http://localhost:5173",
+        "https://job-portal-peach-iota.vercel.app"
+    ],
+    credentials: true
 }
 
 app.use(cors(corsOptions));
